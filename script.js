2506ab37b19d7cd04b4a824353cc454da8cfcbbd
pirvanrobert <pirvanrobert@gmail.com>
2022-12-26 16:22:33 +0200

    Finaly I liked

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-"use strict";
+'use strict';
 
 // Selecting elements
-const player0El = document.querySelector(".player--0");
-const player1El = document.querySelector(".player--1");
-const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("score--1");
-const current0EL = document.getElementById("current--0");
-const current1EL = document.getElementById("current--1");
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
 
-const diceEl = document.querySelector(".dice");
-const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const buttonOpenModal = document.querySelectorAll('.show-modal');
+const buttonCloseModal = document.querySelector('.close-modal');
 
 // Starting conditions
 
@@ -29,11 +32,11 @@ const init = function () {
   current0EL.textContent = 0;
   current1EL.textContent = 0;
 
-  diceEl.classList.add("hidden");
-  player0El.classList.remove("player--winner");
-  player1El.classList.remove("player--winner");
-  player0El.classList.add("player--active");
-  player1El.classList.remove("player--active");
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
 };
 init();
 
@@ -41,18 +44,18 @@ const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle("player--active");
-  player1El.classList.toggle("player--active");
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
 };
 // Rolling dice functionality
 
-btnRoll.addEventListener("click", function () {
+btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     // Display dice
-    diceEl.classList.remove("hidden");
+    diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
 
     // Check for rolled 1
@@ -69,7 +72,7 @@ btnRoll.addEventListener("click", function () {
   }
 });
 
-btnHold.addEventListener("click", function () {
+btnHold.addEventListener('click', function () {
   if (playing) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
@@ -82,19 +85,45 @@ btnHold.addEventListener("click", function () {
     if (scores[activePlayer] >= 100) {
       // Finish Game
       playing = false;
-      diceEl.classList.add("hidden");
-      btns.classList.add("hidden");
+      diceEl.classList.add('hidden');
+      btns.classList.add('hidden');
 
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add("player--winner");
+        .classList.add('player--winner');
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove("player--active");
+        .classList.remove('player--active');
     } else {
       // Switch the player
       switchPlayer();
     }
   }
 });
-btnNew.addEventListener("click", init);
+btnNew.addEventListener('click', init);
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < buttonOpenModal.length; i++) {
+  buttonOpenModal[i].addEventListener('click', openModal);
+  buttonCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+}
+
+window.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
